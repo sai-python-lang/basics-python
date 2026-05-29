2900a8182b44839cdb0590785fa50f9967d7189a
sai-python-dev <saipoojitha517@gmail.com>
2026-05-29 11:50:43 +0530

    Add files via upload
    These small programs provide a solid foundation for beginners to understand Python data types and variables. They can be extended for further practice, such as lists, tuples, dictionaries, and more complex data structures.

---
+++ basics.py
@@ -0,0 +1,46 @@
+# DATA TYPES 
+
+# ADDING TWO NUMBERS 
+a = 9
+b = 2
+print(a+b)
+print("sum = ",a+b)
+
+
+# FINDING DATA TYPE
+data = "string@43"
+print(data,type(data))
+data1 = 1098
+print(data1,type(data1))
+
+# CONVERTING INTEGER TYPE TO FLOAT TYPE
+num = 1000
+print(float(num))
+
+# CONVERTING FLOAT TYPE TO INTEGER TYPE
+num1 = 67.5
+print(int(num1))
+
+# USER INPUT FUNCTION
+name = input("enter your name for recognition:?")
+# take input or the data from user 
+print(name)
+
+#  STRING CONCATENATION
+dog = input("what sound does dog makes :")
+cat = input("what sound does cat makes :")
+print("the sounds are:",dog + cat)
+
+#  SIMPLE CALCULATOR
+num1 = int(input("enter first num:"))
+num2 = int(input("enter second num:"))
+print("adding two numbers is :", num1 + num2)
+
+#  SWAPPING TWO NUMBERS 
+a = 100
+b = 1000
+c = 10000
+a,b,c = c,b,a
+print(a,b,c)
+
+
